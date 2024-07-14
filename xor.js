1ad0e0d84e9ce8178c0fe00e3d237b7f26b51da2
AndreAshmeade <andrelashmeade@gmail.com>
2024-07-13 20:36:23 -0400

    OXR

---
+++ xor.js
@@ -0,0 +1,24 @@
+const brain = require("brain.js");
+
+const config = {
+  inputSize: 20,
+  inputRange: 20,
+  hiddenLayers: [20, 20],
+  outputSize: 20,
+  learningRate: 0.01,
+  decayRate: 0.999,
+};
+
+const network = new brain.recurrent.RNN(config);
+
+network.train([
+  { input: [0, 0, 0], output: [0] },
+  { input: [0, 0, 1], output: [0] },
+  { input: [0, 1, 1], output: [0] },
+  { input: [1, 0, 1], output: [1] },
+  { input: [1, 1, 1], output: [1] },
+]);
+
+const output = network.run([0, 1, 1]);
+
+console.log(output);
